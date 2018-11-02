392146003c2ee0bcc9f39e5f1a88224e4ffcdd73
MadiNaf <madiats2@gmail.com>
2018-11-02 14:42:06 +0000

    homepage test added

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should get root" do
+    #si je vais sur l'url de root
+    get root_path
+    #la reponse doit etre un succee (200) et pas une erreur 404 ou autre
+    assert_response :success
+  end
+  
+  
 end
